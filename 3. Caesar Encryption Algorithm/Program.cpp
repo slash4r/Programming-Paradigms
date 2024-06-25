80e1fcefe116e0eba4a3cbc368ab22b5ec0b9d74
Denys Mykhailov <necuknpocto@gmail.com>
2024-06-25 21:28:23 +0300

    user interface (not the best tho)

--- 3. Caesar Encryption Algorithm/Program.cpp
+++ 3. Caesar Encryption Algorithm/Program.cpp
@@ -2,6 +2,36 @@
 #include <iostream>
 using namespace std;
 
+int const INPUT_LENGTH = 256;
+
+char* get_input() {
+	char* string = new char[INPUT_LENGTH];
+	// get the input from the user
+	char* input = new char[1];
+	input[0] = '\0';
+	while (fgets(string, INPUT_LENGTH, stdin) != NULL)
+	{
+		int new_input_length = strlen(string) + strlen(input);
+
+		char* new_input = new char[new_input_length + 1];
+		strcpy_s(new_input, new_input_length + 1, input);
+		strcat_s(new_input, new_input_length + 1, string);
+
+		delete[] input;
+		input = new_input;
+		// infinite loop here
+
+		if (string[strlen(string) - 1] == '\n')
+		{
+			break;
+		}
+	}
+	// delete string!!!
+	delete[] string;
+	input[strlen(input) - 1] = '\0';
+	return input;
+}
+
 int main()
 {
 	// Pointer to sum function
@@ -29,11 +59,40 @@ int main()
 		return 1;
 	} 
 
-	char string[] = "Roses are red, violets are blue."; // Text to encrypt
-	char* raw_text = string;
-	cout << encr_ptr(raw_text, 3) << endl;
-	cout << decr_ptr(raw_text, 3) << endl;
-
+	while (true)
+	{
+		cout << "Hello! And welcome to Caesar Encryption program" << endl;
+		cout << "Do you want to encrypt or decrypt a message? e/d" << endl;
+		char* choice = get_input();
+		if (!strcmp(choice, "e"))
+		{
+			cout << "Enter the message you want to encrypt" << endl;
+			char* raw_text = get_input();
+			cout << "Enter the key" << endl;
+			int key;
+			cin >> key;
+			cin.ignore();
+			cout << "Encrypted text: " << encr_ptr(raw_text, key) << endl;
+		}
+		else if (!strcmp(choice, "d"))
+		{
+			cout << "Enter the message you want to decrypt" << endl;
+			char* encrypted_text = get_input();
+			cout << "Enter the key" << endl;
+			int key;
+			cin >> key;
+			cin.ignore();
+			cout << "Decrypted text: " << decr_ptr(encrypted_text, key) << endl;
+		}
+		else if (!strcmp(choice, "exit"))
+		{
+			break;
+		}
+		else
+		{
+			cout << "Invalid input! To exit the program enter \"exit\"" << endl;
+		}
+	}
 	FreeLibrary(handle);
 
 
